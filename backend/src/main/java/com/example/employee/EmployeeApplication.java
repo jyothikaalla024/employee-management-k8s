65f52e4c8eac87df7313e17805f09145c2f67a81
jyothikaalla024 <jyothikaalla024@gmail.com>
2026-06-24 11:30:06 +0530

    Update EmployeeApplication.java

--- backend/src/main/java/com/example/employee/EmployeeApplication.java
+++ backend/src/main/java/com/example/employee/EmployeeApplication.java
@@ -1,1 +1,12 @@
-public class EmployeeApplication {}
+package com.example.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeApplication.class, args);
+    }
+}
