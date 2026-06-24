7ef68dd305e8931827ce285e34bea35801690042
jyothikaalla024 <jyothikaalla024@gmail.com>
2026-06-24 11:32:18 +0530

    Implement EmployeeRepository extending JpaRepository

--- backend/src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ backend/src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -1,1 +1,8 @@
-public interface EmployeeRepository {}
+package com.example.employee.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.employee.entity.Employee;
+
+public interface EmployeeRepository
+        extends JpaRepository<Employee, Long> {
+}
